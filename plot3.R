a902201109dd8213948d8dd65f69d3d15dff05c4
Amith Kumar <n.amith1970@gmail.com>
2020-08-24 18:25:15 +0530

    Final commit

---
+++ plot3.R
@@ -0,0 +1,11 @@
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+df <- df[df$Date %in% c("1/2/2007","2/2/2007"),]
+df <- df[complete.cases(df),]
+dateTime <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+df <- cbind(dateTime, df)
+plot(df$dateTime, df$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(df$dateTime, df$Sub_metering_2, type="l", col="red")
+lines(df$dateTime, df$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
